b554adc4f9f45afed8ade91abe4d3f2b3f5f2dca
noname2601 <noname0328@gmail.com>
2021-05-21 19:41:27 +0900

    コントローラー

---
+++ app/controllers/introductions_controller.rb
@@ -0,0 +1,60 @@
+class IntroductionsController < ApplicationController
+  before_action :set_introduction, except: [:index, :new, :create]
+  before_action :authenticate_performer!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
+  def index
+    @introductions = Introduction.includes(:performer)
+    @introductions = Introduction.includes(:user)
+  end
+  
+  def new
+    @introduction = Introduction.new
+  end
+
+  def create
+    @introduction = Introduction.new(introduction_params)
+    if @introduction.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @introduction.update(introduction_params)
+      redirect_to introduction_path(@introduction)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @introduction.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def introduction_params
+    params.require(:introduction).permit(:skill, :description, :day_price, :image).merge(performer_id: current_performer.id)
+  end 
+
+  def set_introduction
+    @introduction = Introduction.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_performer == @introduction.performer
+  end
+end
---
+++ app/controllers/performers_controller.rb
@@ -0,0 +1,6 @@
+class PerformersController < ApplicationController
+  def show
+    @performer = Performer.find(params[:id])
+    @introduction = Introduction.find(params[:id])
+  end
+end
