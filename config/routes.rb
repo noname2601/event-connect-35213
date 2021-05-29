770e15d06c318a382aea4f7500fc51b927bfb982
noname2601 <noname0328@gmail.com>
2021-05-29 22:22:39 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :performers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'introductions#index'
+
+  
+  
   resources :introductions do
   collection do
     get 'search'
@@ -9,5 +13,6 @@ Rails.application.routes.draw do
   end
   
   resources :performers, only: :show
+  resources :messages, only: :create
 end
 
