0ee757517bed3c224baf327a875908f82926b1a3
noname2601 <noname0328@gmail.com>
2021-06-07 22:03:08 +0900

    コントローラー修正

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -2,6 +2,6 @@ class PerformersController < ApplicationController
   def show
     
     @performer = Performer.find(params[:id])
-    #@introduction = Introduction.find(params[:id])
+    @introduction = Introduction.find(params[:id])
   end
 end
