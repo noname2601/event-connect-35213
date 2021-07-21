08f7a7f209dee71c5d813c4ef837ad514354e58a
noname2601 <noname0328@gmail.com>
2021-07-21 13:11:23 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   before_action :find_introduction
 
   def create
-    @like = current_performer.likes.build(introduction_id: params[:introduction_id])
-    @like.save
+    like = current_performer.likes.build(introduction_id: params[:introduction_id])
+    like.save
   end
 
   def destroy
-    @like = Like.find_by(introduction_id: params[:introduction_id], performer_id: current_performer.id)
-    @like.destroy
+    like = Like.find_by(introduction_id: params[:introduction_id], performer_id: current_performer.id)
+    like.destroy
   end
 
   private
